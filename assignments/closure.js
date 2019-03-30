002ff781479a8d8a30116ae65a13cc42c4d3bbee
myasia barbee <myasiabarbee@icloud.com>
2019-03-30 12:48:43 -0400

    Counter function

--- assignments/closure.js
+++ assignments/closure.js
@@ -18,6 +18,17 @@ const counter = () => {
 // newCounter(); // 1
 // newCounter(); // 2
 
+let count = 0;
+return function(){
+  count = count + 1;
+  return count;
+}
+};
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
